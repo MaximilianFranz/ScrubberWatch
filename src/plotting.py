d2bbd3c48f7a1ad3dc0ef364fceb8b72ce6139f0
supermario94123 <supermario94123@gmail.com>
2019-11-23 12:06:28 +0100

    03-UnloadingTimes

---
+++ src/plotting.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+def set_style():
+    plt.style.use("seaborn-whitegrid")
+
+
+NB_RC_PARAMS = {"figure.figsize": [3, 3], "figure.dpi": 220, "figure.autolayout": True}
